c039ddbcc1dd828c0b07c7c2204a5b12c6eb86e3
mfngoi <56236633+mfngoi@users.noreply.github.com>
2024-03-20 14:56:38 -0700

    boron to firebase bit (unfinished)

--- particle_boron/src/particle_boron.cpp
+++ particle_boron/src/particle_boron.cpp
@@ -1,13 +1,16 @@
-/* 
+/*
  * Project myProject
  * Author: Your Name
- * Date: 
+ * Date:
  * For comprehensive documentation and examples, please visit:
  * https://docs.particle.io/firmware/best-practices/firmware-template/
  */
 
 // Include Particle Device OS APIs
+#include <list>
+#include <stdlib.h>
 #include "Particle.h"
+#include "Base64RK.h"
 
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(AUTOMATIC);
@@ -19,17 +22,78 @@ SYSTEM_THREAD(ENABLED);
 // View logs with CLI using 'particle serial monitor --follow'
 SerialLogHandler logHandler(LOG_LEVEL_INFO);
 
-// setup() runs once, when the device is first turned on
-void setup() {
-  // Put initialization like pinMode and begin functions here
+// Forward declarations (functions used before they're implemented)
+int doSomething(String cmd);
+int uploadEKGData(String cmd);
+
+void setup()
+{
+
+  // Particle functions
+  Particle.function("doSomething", doSomething);
+  Particle.function("uploadEKGData", uploadEKGData);
+
+  // initialize the serial communication:
+  Serial.begin(9600);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+void loop()
+{
+}
 
-  // Example: Publish event to cloud every 10 seconds. Uncomment the next 3 lines to try it!
-  // Log.info("Sending Hello World to the cloud!");
-  // Particle.publish("Hello world!");
-  // delay( 10 * 1000 ); // milliseconds and blocking - see docs for more info!
+int uploadEKGData(String cmd)
+{
+  const int size = 1000;
+  short ekgCounter = 0;
+  short ekgSignals[size];
+
+  Serial.println("Collecting 1000 signals");
+  while (ekgCounter < size)
+  {
+
+    short r = rand() % 4096;
+    // Serial.println(r);
+    ekgSignals[ekgCounter] = r;
+
+    delay(1); // Wait for a bit to keep serial data from saturating
+    ekgCounter += 1;
+  }
+  Serial.println("Finished collecting 1000 signals");
+
+
+  // Publish one half of the array
+  // 1. encode half of the array to base64
+  size_t encodedLen = Base64::getEncodedSize(3, true);
+  char *encoded = new char[encodedLen];
+  uint8_t data[] = {50, 224, 208};
+  bool success = Base64::encode(data, 3, encoded, encodedLen, true);
+
+  if(success) {
+    Serial.println(encoded);
+    Serial.println(data[1], BIN);
+    Serial.println(3734, BIN);
+  } else {
+    Serial.println("Something went wrong");
+  }
+  
+  // for (int i = 0; i < ekgCounter; i++)
+  // {
+  //   Serial.println(ekgSignals[i]);
+  // }
+
+  // Serial.println("Amount of Readings: ");
+  // Serial.println(ekgCounter);
+  return 1;
+}
+
+int doSomething(String cmd)
+{
+  int result = 1;
+  Serial.println("doSomething triggered...");
+  if (cmd == "red")
+    Serial.println("Welcome to the matrix");
+  else if (cmd == "blue")
+    Serial.println("Turn away from the truths");
+
+  return result;
 }
