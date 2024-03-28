472a441f5aa3a8e6b9d7bc01d3da29ec7d66f180
Matthew <matthew@Admin-MP.local>
2024-03-28 15:02:12 -0700

    boron example

---
+++ Hardware/finalexample_heart.cpp
@@ -0,0 +1,99 @@
+/*
+ * Project myProject
+ * Author: Your Name
+ * Date:
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+// Include Particle Device OS APIs
+#include <stdlib.h>
+#include <string>
+#include "Particle.h"
+
+using namespace std;
+
+// Let Device OS manage the connection to the Particle Cloud
+SYSTEM_MODE(AUTOMATIC);
+
+// Run the application and system concurrently in separate threads
+SYSTEM_THREAD(ENABLED);
+
+// Show system, cloud connectivity, and application logs over USB
+// View logs with CLI using 'particle serial monitor --follow'
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+// Forward declarations (functions used before they're implemented)
+int doSomething(String cmd);
+int uploadEKGData(String cmd);
+
+void setup()
+{
+
+  // Particle functions
+  Particle.function("doSomething", doSomething);
+  Particle.function("uploadEKGData", uploadEKGData);
+
+  // initialize the serial communication:
+  Serial.begin(9600);
+  pinMode(D0, INPUT); // Setup for leads off detection LO +
+  pinMode(D1, INPUT); // Setup for leads off detection LO -
+}
+
+void loop()
+{
+}
+
+int uploadEKGData(String cmd)
+{
+  const int size = 100;
+
+  // Store ecg signals raw then in 4 bits form
+  unsigned short ekgSignals[size];
+
+  Serial.printf("Collecting %d signals\n", size);
+  int ekgCounter = 0;
+  while (ekgCounter < size)
+  {
+
+    if((digitalRead(D0) == 1)||(digitalRead(D1) == 1)){
+      ekgSignals[ekgCounter] = -1;
+      Serial.println(-1);
+    } else {
+      unsigned short output = analogRead(A0);
+      ekgSignals[ekgCounter] = output
+      Serial.println(output);
+    }
+
+    delay(20); // Wait for a bit to keep serial data from saturating
+    ekgCounter += 1;
+  }
+  Serial.printf("Finished collecting %d signals\n", size);
+
+  string data = "";
+  for (int i=0; i<size; i++) {
+    string num = to_string(ekgSignals[i]); // Convert to string
+    data = data + num + ","
+  }
+
+  // Create JSON
+  string uid = cmd;
+  string signals = data;
+  string data = String::format("{ \"data\": \"%s\", \"uid\": \"%s\" }", signals.c_str(), uid.c_str());
+  // Submit data
+  Particle.publish("PublishTest", data);  // Change to Publish or PublishTest
+
+  return 1;
+}
+
+int doSomething(String cmd)
+{
+  int result = 1;
+  Serial.println("doSomething triggered...");
+  if (cmd == "red")
+    Serial.println("Welcome to the matrix");
+  else if (cmd == "blue")
+    Serial.println("Turn away from the truths");
+
+  return result;
+}
--- firebase/functions/main.py
+++ firebase/functions/main.py
@@ -5,11 +5,13 @@ from firebase_functions import firestore_fn, https_fn, db_fn
 from firebase_admin import initialize_app, firestore, db
 import google.cloud.firestore
 
+# Other modules
+from datetime import datetime
 import base64
-# import bitarray
+import bitarray
+import struct
 
 app = initialize_app()
-
 
 @https_fn.on_request()
 def addmessage(req: https_fn.Request) -> https_fn.Response:
@@ -49,70 +51,67 @@ def makeuppercase(event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None
     upper = original.upper()
     event.data.reference.update({"uppercase": upper})
 
+@db_fn.on_value_created(reference="/particletest/{pushId}")
+def addsignals(event: db_fn.Event[db_fn.Change]) -> None:
 
-@db_fn.on_value_created(reference="/particle/{pushId}")
-async def addsignals(event: db_fn.Event[db_fn.Change]) -> None:
-
+    # Access data
     data = event.data
-    print("Can you see this in the logs")
-    print(f"{data=}")
+    # print("Access dictionary attempt")
+    # print(f"{data=}")
 
-    print("Access dictionary attempt")
-    print(f"{data["coreid"]=}")
-    print(f"{data["data"]=}")
-    print(f"{data["published_at"]=}")
+    # Decode signals base64 into "signals"
+    # base64_string = data["json"]["signals"]
+    # sample_string_bytes = base64.b64decode(base64_string)
+    # b_array = bitarray.bitarray()
+    # b_array = b_array.frombytes(sample_string_bytes)
+    # print(f"{b_array}")
 
-    try:
+    # signals = []    # Array to store decoded values
+    # for i in range(len(b_array)//12):
+    #     start_idx = i * 12
+    #     end_idx = (i+1) * 12
+    #     # print(f"{b_array[start_idx:end_idx]=}")
+    #     num_bits = bitarray.bitarray(b_array[start_idx : end_idx].to01() + ("0"*20), endian='little')
+    #     num = struct.unpack("<L", num_bits)[0]
+    #     signals.append(num)
 
-        # Connect to firestore database
-        firestore_client: google.cloud.firestore.Client = firestore.client()
-        # User information for firestore database
-        userID = "Hv7dlyqYPhYhIOukasD7Yh8G1Du1"
-        weekID = "week11"
-        reportID = data["published_at"]
-        entry = {
-            "signals": data["data"],
-        }
+    # Get correct week number
+    date = datetime.now()
 
-        # Check if weekly_reports exists by counting amount of documents in collection
-        docs = await firestore_client.collection("Users").doc(userID).collection("weekly_reports").stream()
-        length = 0
-        async for doc in docs:
-            length += 1
-        if length == 0:  # If does not exist, initalize it with 0 warnings
-            await firestore_client.collection("Users").doc(userID).collection("weekly_reports").doc(weekID).set({"warnings": 0})
+    weekNum = 0
+    startDay = datetime(date.year, 1, 1)
+    daysDiff = (date - startDay).days
+    if date.weekday == 6:  # Check if it is a sunday
+        weekNum = daysDiff // 7 + 1
+    else: 
+        weekNum = daysDiff // 7
 
-        # Set report
-        _, doc_ref = await firestore_client.collection("users").doc(userID).collection("weekly_reports").doc(weekID).collection("reports").doc(reportID).set(entry)
-    except Exception as error:
-        print("Error found:", error)
+    # Create new document
+    userID = data["json"]["user"]
+    weekID = "week" + str(weekNum)
+    reportID = str(date)
 
+    entry = {
+        "signals": data["json"]["signals"],
+    }
 
+    # Connect to database
+    firestore_client: google.cloud.firestore.Client = firestore.client()
 
+    # Check if weekly_reports document exists
+    doc = firestore_client.collection("users").document(userID).collection("weekly_reports").document(weekID).get()
 
+    # Add week if it does not exists
+    if not doc.exists:
+        print(f"{weekID} did not exist, creating document... ")
+        firestore_client.collection("users").document(userID).collection("weekly_reports").document(weekID).set({"warnings": 0})
 
+    # Push document to database
+    print(f"Added report: {reportID} to user db... ")
+    firestore_client.collection("users").document(userID).collection("weekly_reports").document(weekID).collection("reports").document(reportID).set(entry)
 
-#     # base64_string = data["data"]
-
-#     # sample_string_bytes = base64.b64decode(base64_string)
-#     # print(f"{sample_string_bytes=}")
-
-#     # try:
-#     #     # Manipulating bits
-#     #     ba = bitarray.bitarray()
-#     #     ba.frombytes(sample_string_bytes)
-#     #     print(f"{ba=}")
-#     #     # print(f"{ba.bitarray(1)=}")
-#     # except Exception as error:
-#     #     print("An error occurred:", error)
 
 
 # https://us-central1-test-auth-eaf78.cloudfunctions.net/addmessage?text=uppercasemetoo
 # https://us-central1-eureka-44973.cloudfunctions.net/addmessage?text=uppercasemetoo
-        
-# # 100100000111
-# # 001010110110
-
-# # ChatGPT
-# # Test Case of incoming bit array
-# # 100100000111001010110110010101111010001001010001100101111011110101010111011011001100011101100000100011111100010100010111100001010101001110111110011111110110000111001011000110101010010000101001010101001011101100001011110000011110111100011010010100011010000100111010101000001010110001111110111110111011101010001101110100101000011010011000011011100000100001010100111000110011100110110011100111000011000101101001100110100001100000101100100010011000000010100011010110000001010101110011001011010101000101110110100001110001110000000001110110011010010100110011000111011110100001101101011000111110100001011001110010111110110101111101101111010100110011011111100100110101101101010000011111110101110000010110101011101110110011010000010111100110110010000100101011110010101111110110011001000001010101111000100010001000110011000110111
+    
