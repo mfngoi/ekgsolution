ff2e89a397519c28153871269e3fd31525f13e33
mfngoi <56236633+mfngoi@users.noreply.github.com>
2024-04-16 15:53:39 -0700

    major ui design update and project organization

--- BackendCode/Classifier/neurokit_testing.py
+++ BackendCode/Classifier/neurokit_testing.py
@@ -46,8 +46,8 @@ def avg_qt_interval_reading(signals, info):
 
 # Get 10,000 reading sample from file
 input_data = []
-with open("target/heartsample_5v_100_5sec_03.log", 'r') as file:
-    while len(input_data) < 100:
+with open("target/heartsample_5v_01.log", 'r') as file:
+    while len(input_data) < 10000:
         line = file.readline().strip()
         if line.isdigit():
             input_data.append(int(line))
@@ -61,44 +61,62 @@ input_data_np = np.asarray(input_data, dtype=np.float64)       # convert to type
 # print(input_data_np)
 
 # Process ecg
-ecg_signals, info = nk.ecg_process(input_data_np, sampling_rate=20)
-print(ecg_signals)
+ecg_signals, info = nk.ecg_process(input_data_np, sampling_rate=1000)
+print(f"{ecg_signals=}")
 # print(ecg_signals.columns)
 # ecg_signals['ECG_P_Offsets'].to_csv('hello.csv')
 # print(ecg_signals['ECG_P_Offsets'])
-print(info)
+print(f"{info=}")
+print(f"{info['ECG_P_Onsets']=}")
+print(f"{len(info['ECG_P_Onsets'])=}")
+print(f"{info['ECG_R_Peaks']=}")
+print(f"{len(info['ECG_R_Peaks'])=}")
 
-avg_pr = avg_pr_interval_reading(ecg_signals, info)
+rr_list = []
+for i in range(len(info['ECG_R_Peaks'])-1):
+    curr_rr = info['ECG_R_Peaks'][i+1] - info['ECG_R_Peaks'][i]
+    rr_list.append(curr_rr)
+
+print(f"{rr_list=}")
+print(f"{len(rr_list)=}")
+avg_rr = sum(rr_list) / len(rr_list)
+print(f"{avg_rr=}")
+
+
+# avg_pr = avg_pr_interval_reading(ecg_signals, info)
 avg_qt = avg_qt_interval_reading(ecg_signals, info)
-print(f"{avg_pr=}")
+# print(f"{avg_pr=}")
 print(f"{avg_qt=}")
 
-# Examine plot (Displays ecg analysis overview)s
-nk.ecg_plot(ecg_signals, info)
-plt.show()
+avg_qtc = avg_qt / avg_rr
+print(f"{avg_qtc=}")
 
-# Segments each heart beat from our ecg_signals
-# 1 heartbeat segment per R-Peaks
-heartbeats = nk.ecg_segment(ecg_signals, rpeaks=info["ECG_R_Peaks"], sampling_rate=info["sampling_rate"], show=True)
-# print(heartbeats)
+# # Examine plot (Displays ecg analysis overview)s
+# nk.ecg_plot(ecg_signals, info)
+# plt.show()
+
+# # Segments each heart beat from our ecg_signals
+# # 1 heartbeat segment per R-Peaks
+# heartbeats = nk.ecg_segment(ecg_signals, rpeaks=info["ECG_R_Peaks"], sampling_rate=info["sampling_rate"], show=True)
+# # print(heartbeats)
 
 
-# Convert each segment to its own data frame
-df = epochs_to_df(heartbeats)
+# # Convert each segment to its own data frame
+# df = epochs_to_df(heartbeats)
 
-# Get main signal column name
-# Selects either "Signal", "ECG_Raw", or "ECG_Clean" column to average
-# col = [c for c in ["Signal", "ECG_Raw", "ECG_Clean"] if c in df.columns][-1]
-col = "ECG_Clean"
-print(col)
+# # Get main signal column name
+# # Selects either "Signal", "ECG_Raw", or "ECG_Clean" column to average
+# # col = [c for c in ["Signal", "ECG_Raw", "ECG_Clean"] if c in df.columns][-1]
+# col = "ECG_Clean"
+# print(col)
 
-# Calculate average heartbeat
-mean_heartbeat = df.groupby("Time")[[col]].mean()
-# print(f"{mean_heartbeat=}")
-mean_hb = mean_heartbeat["ECG_Clean"].tolist()
-print(f"{mean_hb=}")
-# mean_heartbeat.to_csv("target/mean_heartbeat.csv", columns=["ECG_Clean"], index=False)
-mean_heartbeat.plot()
+# # Calculate average heartbeat
+# mean_heartbeat = df.groupby("Time")[[col]].mean()
+# # print(f"{mean_heartbeat=}")
+# mean_hb = mean_heartbeat["ECG_Clean"].tolist()
+# print(f"{mean_hb=}")
+# # mean_heartbeat.to_csv("target/mean_heartbeat.csv", columns=["ECG_Clean"], index=False)
+# mean_heartbeat.plot()
 
 # np_mean_heartbeat = mean_heartbeat["ECG_Clean"].to_numpy()
 # np_mean_heartbeat = np.hstack((np_mean_heartbeat, np.zeros(1000)))
