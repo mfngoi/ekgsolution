6f0aa0dbf53352f2257c0c01dfe9b891f6a79e72
mfngoi <56236633+mfngoi@users.noreply.github.com>
2024-04-18 11:14:20 -0700

    sync update

--- BackendCode/Classifier/display_collected_data.py
+++ BackendCode/Classifier/display_collected_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-csv = pd.read_csv(f"subjects/collected_dataset.csv")
+csv = pd.read_csv(f"collected_dataset.csv")
 
 RACE = 'WHITE' # WHITE, ASIAN, AFRICAN AMERICAN
 SEX = 'M' # M, F
@@ -14,32 +14,12 @@ race_sex_csv = race_csv.loc[race_csv['SEX'] == SEX]
 
 plt.title(f"{RACE} {CONDITION} (AVG_P_WAVE vs AGE)")
 plt.xlabel('AGE')
-plt.ylabel('AVG_PR')
-plt.scatter(race_sex_csv['AGE'],race_sex_csv[f"{CONDITION}_PR"])
+plt.ylabel('AVG_P_WAVE')
+plt.scatter(race_sex_csv['AGE'],race_sex_csv[f"{CONDITION}_P_WAVE"])
 plt.show()
 
 plt.title(f"{RACE} {CONDITION} (AVG_QTC vs AGE)")
 plt.xlabel('AGE')
-plt.ylabel('AVG_QT')
-plt.scatter(race_sex_csv['AGE'],race_sex_csv[f"{CONDITION}_QT"])
+plt.ylabel('AVG_QTC')
+plt.scatter(race_sex_csv['AGE'],race_sex_csv[f"{CONDITION}_QTC_INTERVAL"])
 plt.show()
-
-
-# asian_csv = csv.loc[csv['RACE'] == 'ASIAN']
-
-# african_csv = csv.loc[csv['RACE'] == 'AFRICAN AMERICAN']
-
-# Placebo
-    # WHITE
-        # PR vs AGE
-        # QT vs AGE
-
-    # ASIAN
-        # PR vs AGE
-        # QT vs AGE
-
-    # AFRICAN AMERICAN
-        # PR vs AGE
-        # QT vs AGE
-
-
